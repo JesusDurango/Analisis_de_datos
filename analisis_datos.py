411a1014e16c560e884de8e8c80699f6f4533c13
Jesus Abrahan <jesus.abrahanda@gmail.com>
2023-10-24 15:11:25 -0500

    se agrega actividad analisis de datos 1

---
+++ analisis_datos.py
@@ -0,0 +1,12 @@
+import numpy as np
+from datasets import load_dataset
+
+
+dataset = load_dataset("mstz/heart_failure")
+data = dataset["train"]
+
+edades =data["age"]
+
+edades_np = np.array(edades)
+
+promedio_edades = np.mean(edades_np)
